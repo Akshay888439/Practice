3e48a9cea2e05005d7f656baa9ddd83507357386
Akshay888439 <142484422+Akshay888439@users.noreply.github.com>
2023-08-17 20:59:45 +0530

    ControlConstruct
    ControlConstruct

---
+++ SumOfNaturalNumber.java
@@ -0,0 +1,19 @@
+package com.aj.sumofnaturalnumber;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumber {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number = ");
+		int num=scan.nextInt();
+		int sum=0;
+		for(int i=1; i<=num; i++) {
+		  sum=num+i;
+		}
+			System.out.println("Tne sum of " +num+ " natural number = " +sum );
+			scan.close();
+	}
+
+}
